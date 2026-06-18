6940282e0dba990e8518c583eef0b8078c9ea36c
prab-hector <prabedu77@gmail.com>
2026-06-18 22:18:11 +0530

    server created but error, login requrement remove

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegisterForm
 from .models import AttendanceLog  # Assuming AttendanceLog handles the check-ins
 
