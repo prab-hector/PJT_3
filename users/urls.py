ac6ce557b805e1f0ead176d3ee85d5eee4be8931
prab-hector <prabedu77@gmail.com>
2026-06-18 17:49:14 +0530

    2nd draft with urls

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.homepg, name = 'homepg'),
     path('register/',views.register,name = 'register'),
+    path('api/rfid/process/', views.process_rfid, name='api-rfid-process'),
 ]
