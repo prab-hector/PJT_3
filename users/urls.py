2be7b983793f40a6ad94b16387c099797ff76324
prab-hector <prabedu77@gmail.com>
2026-06-20 01:37:00 +0530

    working on login

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # Main landing page dashboard view tracking real-time logs
     # URL: https://nli.pythonanywhere.com/
     path('', views.home_dashboard, name='homepg'),
-    path('login/', views.login, name = 'login').
+    path('login/', views.login, name = 'login'),
     
     # Secure blind student data entry onboarding portal
     # URL: https://nli.pythonanywhere.com/register/
