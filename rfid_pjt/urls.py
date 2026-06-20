39be124804f562e51bed6911092b15b93bec5020
prab-hector <prabedu77@gmail.com>
2026-06-20 12:03:31 +0530

    homepg modific 2

--- rfid_pjt/urls.py
+++ rfid_pjt/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
     path('profile/', user_views.profile, name = 'profile'),
+    path('register/', user_views.register, name='register'),
     path('api/rfid/register-submit/', register_user_submit, name='api-rfid-register-submit'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name = 'password_reset'),
 
