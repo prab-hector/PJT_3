f12a5a5e051779cb264794be26b4998bac1e94dd
prab-hector <prabedu77@gmail.com>
2026-06-21 20:15:10 +0530

    rfid

--- rfid_pjt/urls.py
+++ rfid_pjt/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='users/reset_password.html'), name='reset_password'),
-    path('api-attendancelog/', rf_views.process_rfid, name='process_rfid' )
+    path('api-attendancelog/', rf_views.process_rfid, name='process_rfid' ),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('generate-report/', data_views.export_custom_date_view, name='report'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
