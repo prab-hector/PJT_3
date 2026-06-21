0246eb7f36bd5d56ec2aa37a6fb83da7b325a49a
prab-hector <prabedu77@gmail.com>
2026-06-21 16:30:52 +0530

    homepg

--- rfid_pjt/urls.py
+++ rfid_pjt/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from data_excel import views as data_views
 
 
 urlpatterns = [
@@ -31,7 +32,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='users/reset_password.html'), name='reset_password'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('generate-report/', views.your_report_generation_function, name='generate_report_view_name'),
+    path('generate-report/', data_views.your_report_generation_function, name='generate_report_view_name'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     
     # 2. WEB USER DASHBOARD INTERFACES
     path('', include('users.urls')),
