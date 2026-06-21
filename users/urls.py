0249fac2b707571738936b9d51b20b8b28a4687c
prab-hector <prabedu77@gmail.com>
2026-06-21 22:47:00 +0530

    setpassword

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home_dashboard, name='homepg'),
     path('profile/',views.profile, name='profile'),
     path('profile/<int:pk>/edit/', views.edit_profile, name='edit_profile'),
-    path('set_password//<int:pk>/', views.set_password, name='set_password' ),
+    path('set_password/<int:pk>/', views.set_password, name='set_password'),
     
     # Secure teammate data entry onboarding registration portal
 ]
