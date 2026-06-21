67e1ef8dcee6d7a2c034dd262cb64811798acf83
prab-hector <prabedu77@gmail.com>
2026-06-21 16:38:25 +0530

    homepg

--- rfid_pjt/urls.py
+++ rfid_pjt/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='users/reset_password.html'), name='reset_password'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('generate-report/', data_views.your_report_generation_function, name='generate_report_view_name'),
+    path('generate-report/', data_views.export_current_month_on_demand, name='export_current_month_on_demand'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     
     # 2. WEB USER DASHBOARD INTERFACES
