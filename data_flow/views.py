c0a290f1ff6866e8cd22756b0efd9754875778b8
prab-hector <prabedu77@gmail.com>
2026-06-25 02:01:31 +0530

    master card logic

--- data_flow/views.py
+++ data_flow/views.py
@@ -50,16 +50,6 @@ def _generate_random_username():
         if not User.objects.filter(username=username).exists():
             return username
 
-
-def _generate_unique_username(base_username):
-    username = base_username
-    counter = 1
-    while User.objects.filter(username=username).exists():
-        username = f"{base_username}_{counter}"
-        counter += 1
-    return username
-
-
 @csrf_exempt
 def process_rfid(request):
     if request.method == 'POST':
